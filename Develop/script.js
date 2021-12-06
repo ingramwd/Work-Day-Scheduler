2c43c075b5da7945ea3c336dd19acb2cd885ac54
ingramwd <Ingramwd01@gmail.com>
2021-12-06 18:47:22 -0500

    added save feature to local storage and ability to pull form local storage

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,6 @@
 var currentDateEl = document.querySelector('#Date');
 var p = document.createElement("p");
 var saveButton = document.querySelectorAll(".saveBtn");
-var text = document.querySelectorAll('.description')
 
 // creating date and formatting it into a string
 var date = moment();
@@ -19,17 +18,39 @@ p.textContent = dateFormat;
 
 
 // save button for text element
+$(".saveBtn").on("click", function () {
 
-var saveData = function () {
-    console.log("Hello");
-}
+    // save textarea data into local storage in a key=value format the value here is whatever the user input is for the text area
+    var textarea = $(this).siblings(".description").val();
 
-$(".time").on("click", "button", function (event) {
-    console.log(saveData)
+    // the key value is pulling the id from the div that that value is pulled form
+    var divId = $(this).parent().attr("id");
+
+
+
+    // saves data into the local storage
+    localStorage.setItem(divId, textarea);
 })
 
 
+// getData function retrieves data from the localStorage and sets it to corresponding div to the id and then use that id to set it into 
+// that current textarea "because they all use the description class"
+var getData = function () {
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
+};
 
+getData();
 
+var timeCheck = function () {
+    $(".")
 
+}
 
